d951d159e46835cf26694c3163b2c842bfa4e67f
Nulled <63370961+Nul-led@users.noreply.github.com>
2024-06-07 00:52:25 +0200

    Update solver.rs

--- src/plugins/solver.rs
+++ src/plugins/solver.rs
@@ -194,8 +194,8 @@ fn penetration_constraints(
                         ..*contact
                     };
 
-                    let p1 = body1.current_position() + body1.rotation.rotate(self.contact.point1);
-                    let p2 = body2.current_position() + body2.rotation.rotate(self.contact.point2);
+                    let p1 = body1.current_position() + body1.rotation.rotate(contact.point1);
+                    let p2 = body2.current_position() + body2.rotation.rotate(contact.point2);
                     let penetration = p1 - p2;
                     let penetration_depth = penetration.dot(contact.global_normal1(&body1.rotation));
 
