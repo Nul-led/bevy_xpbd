956dcedd09c6f326343f66786528aef7cb7f41bd
Nulled <63370961+Nul-led@users.noreply.github.com>
2024-06-07 02:12:45 +0200

    Update solver.rs

--- src/plugins/solver.rs
+++ src/plugins/solver.rs
@@ -204,8 +204,8 @@ fn penetration_constraints(
                     }
 
                     let penetration_angle = (body1.current_position() - body2.current_position()).to_angle();
-                    body1.accumulated_translation.0 += Vec2::from_angle(penetration_angle) * (body1.restitution.coefficient * body2.density.0) * delta_secs;
-                    body2.accumulated_translation.0 -= Vec2::from_angle(penetration_angle) * (body2.restitution.coefficient * body1.density.0) * delta_secs;
+                    body1.accumulated_translation.0 += Vec2::from_angle(penetration_angle) * (body1.restitution.coefficient * body2.density.0);
+                    body2.accumulated_translation.0 -= Vec2::from_angle(penetration_angle) * (body2.restitution.coefficient * body1.density.0);
 
                     contacts.during_current_frame = true;
                     contacts.during_current_substep = true;
